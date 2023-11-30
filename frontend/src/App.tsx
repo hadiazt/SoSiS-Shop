29afc7d352572c5d717d730eeb2aeedfa0d61cac
hadi <hadiazari30@gmail.com>
2023-11-30 15:17:12 +0330

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+
+import { HomeScreen } from "./screens/HomeScreen";
 export default function App() {
   return (
     <>
       <Header />
+      <HomeScreen />
       <Footer />
     </>
   );
