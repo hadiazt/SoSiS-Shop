b50c9fea35beb6261ba6d99bbf4916075996cae7
hadi <hadiazari30@gmail.com>
2023-12-01 09:38:50 +0330

    use react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
+import { Outlet } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 
-import { HomeScreen } from "./screens/HomeScreen";
 export default function App() {
   return (
     <>
       <Header />
-      <HomeScreen />
+      <Outlet />
       <Footer />
     </>
   );
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
+import { HomeScreen } from "./screens/HomeScreen";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />}></Route>
+    </Route>
+  )
+);
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
