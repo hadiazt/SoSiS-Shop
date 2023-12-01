e4243b7f8a59ab0d27cfad145c9635909580020d
hadi <hadiazari30@gmail.com>
2023-12-01 22:13:08 +0330

    Update Product.tsx

--- frontend/src/components/Product.tsx
+++ frontend/src/components/Product.tsx
@@ -12,11 +12,8 @@ export const Product = () => {
               _id: string;
               name: string;
               image: string;
-              description: string;
-              brand: string;
               category: string;
               price: number;
-              countInStock: number;
               rating: number;
               numReviews: number;
             }) => (
