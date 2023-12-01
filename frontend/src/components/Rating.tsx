f0a79a8fb55e1d8bb4986113ac7dcfbc2e0ce830
hadi <hadiazari30@gmail.com>
2023-12-01 11:23:46 +0330

    add rating com

--- frontend/src/components/Rating.tsx
+++ frontend/src/components/Rating.tsx
@@ -1,3 +1,16 @@
+import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
+
 export const Rating = ({ Rate, Review }: { Rate: number; Review: number }) => {
-    return <p className="mt-1 text-sm text-gray-500">{Rate + Review}</p>;
-  };
+  return (
+    <div className="flex text-sm mt-1">
+      <div className="flex items-center text-yellow-500">
+        {Rate >= 1 ? <FaStar /> : Rate >= 0.5 ? <FaStarHalfAlt /> : <FaRegStar />}
+        {Rate >= 2 ? <FaStar /> : Rate >= 1.5 ? <FaStarHalfAlt /> : <FaRegStar />}
+        {Rate >= 3 ? <FaStar /> : Rate >= 2.5 ? <FaStarHalfAlt /> : <FaRegStar />}
+        {Rate >= 4 ? <FaStar /> : Rate >= 3.5 ? <FaStarHalfAlt /> : <FaRegStar />}
+        {Rate >= 5 ? <FaStar /> : Rate >= 4.5 ? <FaStarHalfAlt /> : <FaRegStar />}
+      </div>
+      <p className="ml-1">{Review} Reviews</p>
+    </div>
+  );
+};
