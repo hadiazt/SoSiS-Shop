02aff613f670641e942c43294cf527e1b5f3f115
hadi <hadiazari30@gmail.com>
2023-12-01 09:38:37 +0330

    Update Product.tsx

--- frontend/src/components/Product.tsx
+++ frontend/src/components/Product.tsx
@@ -3,10 +3,13 @@ import { products } from "../products";
 export const Product = () => {
   return (
     <div className="bg-white">
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
-            <div key={product._id} className="group relative p-2 rounded-xl shadow-xl transition ease-in-out delay-150 bg-gray-200 hover:-translate-y-1 hover:scale-110 hover:bg-white duration-500">
+            <div
+              key={product._id}
+              className="group relative p-2 rounded-xl shadow-xl transition ease-in-out delay-150 bg-gray-200 hover:-translate-y-1 hover:scale-110 hover:bg-gray-100 duration-500"
+            >
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none lg:h-80">
                 <img
                   src={product.image}
