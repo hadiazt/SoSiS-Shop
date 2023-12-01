731e9da2592e6c4a4f283ddc7f7ee7720a8e17d9
hadi <hadiazari30@gmail.com>
2023-12-01 12:17:06 +0330

    make footer fixed

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,8 @@
 export const Footer = () => {
   const currentYear = new Date().getFullYear();
-
   return (
-    <footer className="text-center">SoSiS Shop &copy; {currentYear}</footer>
+    <footer className="fixed bottom-5 w-full text-center">
+      SoSiS Shop &copy; {currentYear}
+    </footer>
   );
 };
