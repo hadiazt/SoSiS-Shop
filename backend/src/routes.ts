be33203f2a69cba3189940c8ede19f87ec82a3ee
hadi <hadiazari30@gmail.com>
2023-12-02 21:39:13 +0330

    Update product routes to use req and res with type any

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,14 +3,14 @@ import { DB } from './db/connector';
 
 export const Routes = (app: { get: Function }) => {
     DB()
-    app.get('/api/v1', (req, res) => {
+    app.get('/api/v1', (req: any, res: any) => {
         res.send('Hello World!');
     });
-    app.get('/api/v1/product', (req, res) => {
+    app.get('/api/v1/product', (req: any, res: any) => {
         res.json(products)
     });
-    app.get('/api/v1/product/:id', (req, res) => {
-        const Product = products.find((p: { _id: string }) => p._id === req.params.id);
-        res.json(Product)
+    app.get('/api/v1/product/:id', (req: any, res: any) => {
+        // const Product = products.find((p: { _id: string }) => p._id === req.params.id);
+        res.json(products)
     });
 }
