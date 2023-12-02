6418ce8bdd12b44b7f7d93eea2ba4fc77d8f5e3b
hadi <hadiazari30@gmail.com>
2023-12-03 00:08:10 +0330

    Update response type for product router

--- backend/src/routes/products.ts
+++ backend/src/routes/products.ts
@@ -2,6 +2,9 @@ import { Router } from "express";
 
 export const ProductRouter = Router()
 
-ProductRouter.get('/', (req, res) => {
+ProductRouter.get('/', (req, res: { status: Function, json: Function }) => {
+    console.log('a');
+})
+ProductRouter.get('/:id', (req, res) => {
     console.log('a');
 })
