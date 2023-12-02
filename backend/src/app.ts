417e638492dd3455566457466d1e0dd005cd5b48
hadi <hadiazari30@gmail.com>
2023-12-02 14:59:01 +0330

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,23 +2,12 @@ import RateLimit from 'express-rate-limit';
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import { Routes } from './routes';
 
-import { products } from './data/products'
 const PORT = process.env.PORT;
 const app = express();
 app.use(cors());
-app.get('/api/v1', (req, res) => {
-    res.send('Hello World!');
-});
-app.get('/api/v1/product', (req, res) => {
-    res.json(products)
-});
-app.get('/api/v1/product/:id', (req, res) => {
-    const Product = products.find((p: { _id: string }) => p._id === req.params.id);
-    res.json(Product)
-});
-
-
+Routes(app)
 app.listen(PORT, () => {
     console.log('Server Is Running On Port ' + PORT);
 })
