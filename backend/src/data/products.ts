136f77255f2a1dfb37c138708e8c03af88125c84
hadi <hadiazari30@gmail.com>
2023-12-02 19:48:53 +0330

    Update product data model

--- backend/src/data/products.ts
+++ backend/src/data/products.ts
@@ -1,97 +1,97 @@
 export const products = [
   {
     _id: '1',
-    name: 'Airpods Wireless Bluetooth Headphones',
-    image: '/images/airpods.jpg',
-    description:
+    Name: 'Airpods Wireless Bluetooth Headphones',
+    IMG: '/IMGs/airpods.jpg',
+    Description:
       'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
-    brand: 'Apple',
-    category: 'Electronics',
-    price: 89.99,
-    countInStock: 10,
-    rating: 4.5,
-    numReviews: 12,
-    features: [
+    Brand: 'Apple',
+    Category: 'Electronics',
+    Price: 89.99,
+    CountInStock: 10,
+    Rating: 4.5,
+    NumReviews: 12,
+    Features: [
       {
         Name: "Factor",
         Des: "ATX"
       }
     ],
-    warranty: 0
+    Warranty: 0
   },
   {
     _id: '2',
-    name: 'iPhone 11 Pro 256GB Memory',
-    image: '/images/phone.jpg',
-    description:
+    Name: 'iPhone 11 Pro 256GB Memory',
+    IMG: '/IMGs/phone.jpg',
+    Description:
       'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
-    brand: 'Apple',
-    category: 'Electronics',
-    price: 599.99,
-    countInStock: 0,
-    rating: 4.0,
-    numReviews: 8,
-    features: [],
-    warranty: 0
+    Brand: 'Apple',
+    Category: 'Electronics',
+    Price: 599.99,
+    CountInStock: 0,
+    Rating: 4.0,
+    NumReviews: 8,
+    Features: [],
+    Warranty: 0
   },
   {
     _id: '3',
-    name: 'Cannon EOS 80D DSLR Camera',
-    image: '/images/camera.jpg',
-    description:
+    Name: 'Cannon EOS 80D DSLR Camera',
+    IMG: '/IMGs/camera.jpg',
+    Description:
       'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
-    brand: 'Cannon',
-    category: 'Electronics',
-    price: 929.99,
-    countInStock: 5,
-    rating: 3,
-    numReviews: 12,
-    features: [],
-    warranty: 0
+    Brand: 'Cannon',
+    Category: 'Electronics',
+    Price: 929.99,
+    CountInStock: 5,
+    Rating: 3,
+    NumReviews: 12,
+    Features: [],
+    Warranty: 0
   },
   {
     _id: '4',
-    name: 'Sony Playstation 4 Pro White Version',
-    image: '/images/playstation.jpg',
-    description:
+    Name: 'Sony Playstation 4 Pro White Version',
+    IMG: '/IMGs/playstation.jpg',
+    Description:
       'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
-    brand: 'Sony',
-    category: 'Electronics',
-    price: 399.99,
-    countInStock: 11,
-    rating: 5,
-    numReviews: 12,
-    features: [],
-    warranty: 0
+    Brand: 'Sony',
+    Category: 'Electronics',
+    Price: 399.99,
+    CountInStock: 11,
+    Rating: 5,
+    NumReviews: 12,
+    Features: [],
+    Warranty: 0
   },
   {
     _id: '5',
-    name: 'Logitech G-Series Gaming Mouse',
-    image: '/images/mouse.jpg',
-    description:
+    Name: 'Logitech G-Series Gaming Mouse',
+    IMG: '/IMGs/mouse.jpg',
+    Description:
       'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
-    brand: 'Logitech',
-    category: 'Electronics',
-    price: 49.99,
-    countInStock: 7,
-    rating: 3.5,
-    numReviews: 10,
-    features: [],
-    warranty: 0
+    Brand: 'Logitech',
+    Category: 'Electronics',
+    Price: 49.99,
+    CountInStock: 7,
+    Rating: 3.5,
+    NumReviews: 10,
+    Features: [],
+    Warranty: 0
   },
   {
     _id: '6',
-    name: 'Amazon Echo Dot 3rd Generation',
-    image: '/images/alexa.jpg',
-    description:
+    Name: 'Amazon Echo Dot 3rd Generation',
+    IMG: '/IMGs/alexa.jpg',
+    Description:
       'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
-    brand: 'Amazon',
-    category: 'Electronics',
-    price: 29.99,
-    countInStock: 0,
-    rating: 4,
-    numReviews: 12,
-    features: [],
-    warranty: 0
+    Brand: 'Amazon',
+    Category: 'Electronics',
+    Price: 29.99,
+    CountInStock: 0,
+    Rating: 4,
+    NumReviews: 12,
+    Features: [],
+    Warranty: 0
   },
 ]
--- frontend/src/types/products.ts
+++ frontend/src/types/products.ts
@@ -1,17 +1,17 @@
 export type ProductInfo = {
   _id: string;
-  name: string;
-  image: string;
-  description: string;
-  brand: string;
-  category: string;
-  price: number;
-  countInStock: number;
-  rating: number;
-  numReviews: number;
-  features: Array<{
+  Name: string;
+  IMG: string;
+  Description: string;
+  Brand: string;
+  Category: string;
+  Price: number;
+  CountInStock: number;
+  Rating: number;
+  NumReviews: number;
+  Features: Array<{
     Name: string;
     Des: string;
   }>;
-  warranty: number;
+  Warranty: number;
 };
